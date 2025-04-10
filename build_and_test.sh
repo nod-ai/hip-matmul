0bc43dc6a1a37c23abe52aa7383be453f77f4332
Jakub Kuderski <jakub.kuderski@amd.com>
2025-04-09 22:15:54 -0500

    Add initial matvec kernel
    This is a reference matvec implementation only. The common code shared
    with the data-tiled mmt is moved to `common.hip`.
    
    The matvec scaffolding is based off the mmt code, but without data
    tiling and much of the dynamism (virtual functions / RTTI).

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-set -eux
+set -euxo pipefail
 
 if [[ ! -d build ]]
 then
   mkdir build
 fi
 
-executable=build/hip-matmul
+declare -a KERNELS_TO_RUN=("matmul" "matvec")
 
-hipcc matmul.hip -std=c++20 -Wall -Wextra -O3 -o "${executable}" -save-temps=obj
+if [ ! -z ${KERNEL+x} ]; then
+  KERNELS_TO_RUN=("${KERNEL}")
+fi
 
-"${executable}"
+for kernel in "${KERNELS_TO_RUN[@]}" ; do
+  echo "Build and test: ${kernel}"
+  executable="build/hip-${kernel}"
+  hipcc "${kernel}.hip" -std=c++20 -Wall -Wextra -O3 -o "${executable}" -save-temps=obj
+  "${executable}"
+done

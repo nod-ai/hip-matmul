0139ed13c7df34fc56faf31cee50369e5ef471a3
Jakub Kuderski <jakub.kuderski@amd.com>
2025-04-16 20:09:03 -0500

    Address comments

--- build_and_test.sh
+++ build_and_test.sh
@@ -7,13 +7,9 @@ then
   mkdir build
 fi
 
-declare -a KERNELS_TO_RUN=("matmul" "matvec")
+readonly KERNELS_TO_RUN="${KERNELS_TO_RUN:-matmul matvec}"
 
-if [ ! -z ${KERNEL+x} ]; then
-  KERNELS_TO_RUN=("${KERNEL}")
-fi
-
-for kernel in "${KERNELS_TO_RUN[@]}" ; do
+for kernel in $KERNELS_TO_RUN; do
   echo "Build and test: ${kernel}"
   executable="build/hip-${kernel}"
   hipcc "${kernel}.hip" -std=c++20 -Wall -Wextra -O3 -o "${executable}" -save-temps=obj
